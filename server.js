387399c0846083ec870d2aa6d56dae8c2e0fa9e4
Michael Johnson <mikeeasy11235@gmail.com>
2025-12-10 05:46:31 +0000

    Completed fsLab assignment

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-//call back function
-serveStatic = function (req, res) {
-	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
-}
- 
-const myserver = http.createServer(serveStatic); //create a server object
-myserver.listen(80, function() {console.log("Listening on port 80" )}); 
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            return res.end("File not found");
+        }
+
+        res.writeHead(200, { "Content-Type": "image/jpg" });
+        res.write(data);
+        return res.end();
+    });
+
+}).listen(3000);
+
+console.log("Image server running on port 3000");
 
